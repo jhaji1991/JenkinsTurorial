782dee21c5bed654fdf3eae308e6fee54017e5a5
Deepak Kumar Jha <deepakjha2206@gmail.com>
2024-08-13 10:09:59 +0530

    Initial Commit
    Initial commit first time

---
+++ PracticeProgram/src/com/java/program/Test.java
@@ -0,0 +1,13 @@
+package com.java.program;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("Hello Jenkins");
+		System.out.println("Date    "  + new Date());
+	}
+	
+}
